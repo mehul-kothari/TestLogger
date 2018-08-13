3b1d6815ac0c228fd80f510b827ec14afbc94040
mehul-kothari <mehulkothari1996@gmail.com>
2018-08-13 22:54:26 +0530

    logger in java

---
+++ src/logging/UseLogger.java
@@ -0,0 +1,46 @@
+package logging;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import logging.MyLogger;
+
+public class UseLogger {
+    // use the classname for the logger, this way you can refactor
+    private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+
+    public void doSomeThingAndLog() {
+        System.out.println("lets see what this logger does");
+
+        // now we demo the logging
+
+        // set the LogLevel to Severe, only severe Messages will be written
+        LOGGER.setLevel(Level.SEVERE);
+        LOGGER.severe("Info Log");
+        LOGGER.warning("Info Log");
+        LOGGER.info("Info Log");
+        LOGGER.finest("Really not important");
+        LOGGER.severe("hey guys whats up");
+
+        // set the LogLevel to Info, severe, warning and info will be written
+        // finest is still not written
+        LOGGER.setLevel(Level.INFO);
+        LOGGER.severe("Info Log");
+        LOGGER.warning("Info Log");
+        LOGGER.info("Info Log");
+        LOGGER.finest("Really not important");
+    }
+
+    public static void main(String[] args) {
+        UseLogger tester = new UseLogger();
+        try {
+            MyLogger.setup();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Problems with creating the log files");
+        }
+        tester.doSomeThingAndLog();
+    }
+}
